0697cfa53296e880d4a30240e9732f1165671fef
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2025-01-18 23:34:35 +0100

    swap operations

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,10 +6,11 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 19:27:14 by jalcausa          #+#    #+#             */
-/*   Updated: 2025/01/18 19:59:38 by jalcausa         ###   ########.fr       */
+/*   Updated: 2025/01/18 23:34:21 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 /* Double linked list */
 typedef struct s_node
 {
@@ -18,3 +19,10 @@ typedef struct s_node
 	struct s_node	*prev;
 	struct s_node	*next;
 }	t_node;
+
+/* Operations */
+void	pa(t_node **stack_a, t_node **stack_b);
+void	pb(t_node **stack_a, t_node **stack_b);
+void	sa(t_node **stack_a);
+void	sb(t_node **stack_b);
+void	ss(t_node **stack_a, t_node **stack_b);
---
+++ src/operations/swap.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 23:13:44 by jalcausa          #+#    #+#             */
+/*   Updated: 2025/01/18 23:34:04 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+void	sa(t_node **stack_a)
+{
+	int	aux_val;
+	int	aux_index;
+	
+	if (!stack_a)
+		return ;
+	if (!*stack_a || !((*stack_a)->next))
+		return ;
+	aux_val = (*stack_a)->value;
+	aux_index = (*stack_a)->index;
+	(*stack_a)->value = (*stack_a)->next->value;
+	(*stack_a)->index = (*stack_a)->next->index;
+	(*stack_a)->next->value = aux_val;
+	(*stack_a)->next->index = aux_index;
+}
+
+void	sb(t_node **stack_b)
+{
+	int	aux_val;
+	int	aux_index;
+	
+	if (!stack_b)
+		return ;
+	if (!*stack_b || !((*stack_b)->next))
+		return ;
+	aux_val = (*stack_b)->value;
+	aux_index = (*stack_b)->index;
+	(*stack_b)->value = (*stack_b)->next->value;
+	(*stack_b)->index = (*stack_b)->next->index;
+	(*stack_b)->next->value = aux_val;
+	(*stack_b)->next->index = aux_index;
+}
+
+void	ss(t_node **stack_a, t_node **stack_b)
+{
+	sa(stack_a);
+	sb(stack_b);
+}
